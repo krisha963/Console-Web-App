92ae2e97fa236aed15a4cac0c72d6fe9b022e344
krisha963 <89096112+krisha963@users.noreply.github.com>
2021-11-22 20:00:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+first_name.concat(last_name);
+var fullname = first_name.concat(last_name);
+console.log(fullname);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+var subtract = a - b;
+console.log(subtract);
+var multiply = a * b;
+console.log(multiply);
+var divide = a / b;
+console.log(divide);
